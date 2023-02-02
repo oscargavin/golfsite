dba46a260abf350ff96a1e90db3a5ae4e6ca067f
oscargavin <oscarfgavin@gmail.com>
2023-02-02 18:04:34 +0000

    Removed script tag

--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -9,9 +9,6 @@ import Map from "@/components/Map";
 const Courses: NextPage = () => {
     return (
         <div className="gradient-background h-screen">
-            <Script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
-                integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
-                crossOrigin=""></Script>
             <Header />
             <div className="flex justify-center mt-16">
                 <div className="w-2/5 rounded-md overflow-hidden">
